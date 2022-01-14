f55bf9cf3793c9466c5e1ac766290e8748a3deee
한관수 <jin9776@gmail.com>
2022-01-14 17:25:29 +0900

    Initial commit

---
+++ src/main/java/sol/home/solidhome/SolidHomeApplication.java
@@ -0,0 +1,13 @@
+package sol.home.solidhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolidHomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sol/home/solidhome/SolidHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package sol.home.solidhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolidHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
